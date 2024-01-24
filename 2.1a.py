270afd7db214c1ddbe420361cfa1545f0766e097
beaulee10 <157264109+beaulee10@users.noreply.github.com>
2024-01-24 12:16:10 -0600

    Add files via upload

---
+++ 2.1a.py
@@ -0,0 +1,9 @@
+# Knock-Knock Code!
+print("Knock, knock.")  
+print("Who's there?")  
+print("Candice.")  
+print("Candice who?")  
+print("Candice joke end because I really want to cry!")  
+
+# /n helps seperate the lines when the code is ran
+print("Knock, knock.\nWho's there?\nCandice.\nCandice who?\nCandice joke end because I really want to cry!")
