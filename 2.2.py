9e777cbf0efcaa62854cd3ff01d6bf7869be499b
beaulee10 <157264109+beaulee10@users.noreply.github.com>
2024-01-24 12:45:23 -0600

    Add files via upload

---
+++ 2.2.py
@@ -0,0 +1,19 @@
+# Literal
+x = 48
+print("Literal:", x)
+
+# Integer
+y = -9
+print("Integer:", y)
+
+# Float
+z = 124.8
+print("Float:", z)
+
+# String
+name = "Pam"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
