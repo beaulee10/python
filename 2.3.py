d400f41e43630ff919d46804ba9ca07cc0be6aba
beaulee10 <157264109+beaulee10@users.noreply.github.com>
2024-01-25 12:04:33 -0600

    Add files via upload

---
+++ 2.3.py
@@ -0,0 +1,6 @@
+# Demonstrating the calculation of the area of a rectangle
+length = 12
+width = 8
+area = length * width
+
+print("The area of the rectangle with a length of " + str(length) + " and a width of " + str(width) + " is " + str(area) + ".")
