eaf300ee09483648c41f3a6a72799d6b8f8affba
beaulee10 <157264109+beaulee10@users.noreply.github.com>
2024-01-30 16:52:08 -0600

    Update 2.5.py

--- 2.5.py
+++ 2.5.py
@@ -1,4 +1,8 @@
+#This line defines the size of the tank and how many gallons it can hold
 tank_size = 15
+#Defines the distance to travel
 distance = 600
+#Converts and calculates the mpg
 mpg = distance / tank_size
+#Outputs the results
 print("Miles per gallon:", mpg)
