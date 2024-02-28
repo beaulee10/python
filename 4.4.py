0057858168c229fe8c07c8d5a854b6504dceb735
beaulee10 <157264109+beaulee10@users.noreply.github.com>
2024-02-28 12:02:02 -0600

    4.4.py

--- 4.4.py
+++ 4.4.py
@@ -1,16 +1,16 @@
-POUND = 0.453592
-INCH = 0.0254
+POUND = 0.453592 #constant for the pound to kg conversion
+INCH = 0.0254 #constant for the inches to meter conversion
 
-weight = float(input("Enter your weight in pounds: "))
-height = float(input("Enter your height in inches: "))
+weight = float(input("Enter your weight in pounds: ")) #Will ask the user to input their weight in lbs
+height = float(input("Enter your height in inches: ")) #will ask user to input their height in inches
 
-kilograms = POUND * weight
-meters = INCH * height
+kilograms = POUND * weight #This will convert input from pound to kg
+meters = INCH * height #This will convert input from inches to meters
 
-bmi = kilograms / (meters * meters)
-print(f"Your bmi is {bmi}")
+bmi = kilograms / (meters * meters) #calculates the BMI
+print(f"Your bmi is {bmi}") #prints it
 
-if (bmi < 18.5):
+if (bmi < 18.5): #Each if and elif loop will sort out the users calculated BMI to understand and display what weight category they are in
     print("You are in the underweight category.")
 elif(18.5 <= bmi <= 24.9):
     print("You are in the normal weight category.")
