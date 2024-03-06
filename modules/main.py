716f0ea670a9af6e85d80e2bafbc9c4cf8c18777
beaulee10 <157264109+beaulee10@users.noreply.github.com>
2024-03-06 12:26:03 -0600

    Add files via upload

---
+++ modules/main.py
@@ -0,0 +1,18 @@
+from math_operations import calculator
+
+result = calculator.add(5, 3)
+print("Addition result:", result)
+
+result = calculator.subtract(10, 4)
+print("Subtraction result:", result)
+
+from math_operations import geometric
+
+result = geometric.areaTriangle(5,7)
+print("Area of triangle is: ", result)
+
+result = geometric.areaRectangle(10, 17)
+print("Area of rectangle is: ", result)
+
+result = geometric.areaCircle(6)
+print("Area of circle is: ", result)
---
+++ modules/math_operations/geometric.py
@@ -0,0 +1,8 @@
+def areaTriangle(b, h):
+    return (0.5*b) * h
+
+def areaRectangle(b, h):
+    return b * h
+
+def areaCircle(r):
+    return 3.141592653589 * r * r
