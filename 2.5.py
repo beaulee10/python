178675368cfc6fe4100ad5f5592735c80f88524b
beaulee10 <157264109+beaulee10@users.noreply.github.com>
2024-03-31 21:40:16 -0500

    Add files via upload

--- 2.5.py
+++ 2.5.py
@@ -1,8 +1,63 @@
-#This line defines the size of the tank and how many gallons it can hold
-tank_size = 15
-#Defines the distance to travel
-distance = 600
-#Converts and calculates the mpg
-mpg = distance / tank_size
-#Outputs the results
-print("Miles per gallon:", mpg)
+def main():
+    valid = False
+    while not valid:
+        valid = True
+        print("""Password must be: \n
+            Between 8 to 20 characters long.
+            Contains at least one uppercase letter.
+            Contains at least one lowercase letter.
+            Includes at least one number.
+            Includes at least one symbol from the set: !@#$%&*.""")
+        
+        password = input("Ensure the password meets the following criteria: ")
+        length = len(password)
+        if 8 < length < 20:
+            continue
+        else:
+            valid = False
+            print("Password does not match length requirements")
+
+        is_upper = False
+        is_lower = False
+        for char in password:
+            if char.isalpha():
+                is_upper == True
+            elif char.islower():
+                is_lower == True
+            if is_lower == False or is_upper == False:
+                print("You need to include an upper case and lower case symbol")
+                valid = False
+                continue
+
+
+        includes_symbol = False
+        symbol = ['!', '@', '#']
+        for s in symbol:
+            for c in password:
+                if s == c:
+                    includes_symbol == True
+        if includes_symbol == False:
+            print("You need to include a symbol")
+            valid = False
+            continue
+
+        includes_number = False
+        for int in password:
+            if int(password):
+                includes_number == True
+        if includes_number == False:
+            print("You need to include numbers")
+            valid = False
+            continue
+
+        confirm_password = input("Confirm password: ")
+        if password != confirm_password:
+            raise ValueError("Passwords do not match.")
+
+        print("Success! Password set.")
+        break
+
+main()
+
+
+        
