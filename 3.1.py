5f9b2e9c54df14501a620d45633db2f47ff9974c
beaulee10 <157264109+beaulee10@users.noreply.github.com>
2024-04-15 23:24:47 -0500

    Add files via upload

--- 3.1.py
+++ 3.1.py
@@ -1,22 +1,21 @@
-#Tis will ask user for age 
-age = float(input("How old are you? "))
-#Will determine if they are old enough to drive
-if age >= 16:
-    print("You are old enough to drive!")
-else:
-    print("You are not old enough to drive.")
-#Will determine if they are old enough to vote
-if age >= 21:
-    print("You are old enough to vote!")
-else:
-    print("You are not old enough to vote.")
-#Will determine if they can purchase alcohol
-if age >= 21:
-    print("You are old enough to legally purchase alcohol!")
-else:
-    print("You are not old enough to legally purchase alcohol.")
-#This will determine if they are eligible for senior dicount
-if age>= 65:
-    print("You are eligible for senior dicount!")
-else:
-    print("You are not eligible for senior discount.")
+def table(class_name, attributes, methods):
+    print("|---------------------------------------|")
+    print(f"| Class Name         {class_name.capitalize()}           |")
+    print("|---------------------------------------|")
+    print("| Variables                             |")
+    for atr in attributes:
+        print(f"|                    -{atr}     |")
+    print("|---------------------------------------|")
+    print("| Methods                               |")
+    for mth in methods:
+        print(f"|                    +{mth} |")
+    print("|---------------------------------------|")
+
+def main():
+    class_name = "CoffePot"
+    attributes = ["brand: String", "color: String", "space: Integer", "water_level: Integer"]
+    methods = ["brewCoffee(): void", "setTimer(time): void", "checkWaterLevel(): Integer", "turnOff(): void"]
+
+    table(class_name, attributes, methods)
+
+main()
