94b71fa0e4123317b72bb9d7b8241f433038da02
beaulee10 <157264109+beaulee10@users.noreply.github.com>
2024-04-18 16:03:39 -0500

    Update PE2_4.2.py

--- PE2_4.2.py
+++ PE2_4.2.py
@@ -1,4 +1,4 @@
-def two_letter(characters):
+def two_letter_combinations(characters):
     for char1 in characters:
         for char2 in characters:
             yield char1 + char2
@@ -7,7 +7,7 @@ def main():
     characters = ['a', 'b', 'c']
 
     print("Combinations for two letters:")
-    for combination in two_letter(characters):
+    for combination in two_letter_combinations(characters):
         print(combination)
 
 main()
